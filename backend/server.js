d2ebd51a1420c45618165233b6f9eace4a7a68d7
Mukilanbalakrishnan <154340556+Mukilanbalakrishnan@users.noreply.github.com>
2025-04-24 15:22:14 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,15 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 
+
 const app = express();
+
+
 app.use(cors());
 
+
 const server = http.createServer(app);
+
 
 const io = new Server(server, {
   cors: {
@@ -14,19 +19,30 @@ const io = new Server(server, {
   },
 });
 
+
+// socket connection
+
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
 
+
   socket.on("send_message", (data) => {
     console.log("Message received:", data);
-    io.emit("receive_message", data); // send to all clients
+
+    io.emit("receive_message", data);
   });
+
+
+  // socket disconnection
 
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
   });
 });
 
+
+
+// to start the server on port number 3001
 server.listen(3001, () => {
   console.log("🚀 Server running on http://localhost:3001");
 });
